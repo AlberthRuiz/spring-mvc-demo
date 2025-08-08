7dcdb55fb4934e8aca6699aa160a932ce7151006
CMENDOZA <c@mail.com>
2025-08-07 19:44:03 -0500

    Product service with crud methods completed.

--- src/main/java/edu/pe/cibertec/spring_mvc_demo/service/ProductService.java
+++ src/main/java/edu/pe/cibertec/spring_mvc_demo/service/ProductService.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -24,5 +27,42 @@ public class ProductService {
         }
         return  productRepository.save(product);
     }
+    public List<Product> getAllProduct(){
+        List<Product> products = new ArrayList<>();
+        productRepository.findAll().forEach(products::add);
+        return products;
+    }
 
+    public Product updateProduct(Long id, Product product){
+        Optional<Product> objProduct = productRepository.findById(id);
+        if (objProduct== null){
+            throw  new IllegalArgumentException("Producto no encontrado");
+        }
+        Product update_product = objProduct.get();
+        if(product.getNombre() != null || !product.getNombre().trim().isEmpty()){
+            update_product.setNombre(product.getNombre());
+        }
+        if(product.getPrecio() != null || product.getPrecio().compareTo(BigDecimal.ZERO) >=0){
+            update_product.setPrecio(product.getPrecio());
+        }
+        if(product.getStock() != null || product.getStock() >=0){
+            update_product.setStock(product.getStock());
+        }
+        if(product.getCategoria() != null || !product.getCategoria().trim().isEmpty()){
+            update_product.setCategoria(product.getCategoria());
+        }
+        update_product.setEstado(update_product.isAvailable());
+        return productRepository.save(update_product);
+    }
+
+    public boolean deleteProduct(Long id){
+        if(id == null || id<=0){
+            throw  new IllegalArgumentException("ID no valido");
+        }
+        if(productRepository.existsById(id)){
+            productRepository.deleteById(id);
+            return  true;
+        }
+        return false;
+    }
 }
