ccd98df41ef9339e503db15c49ca0eaa25b0ccb9
SUITE <mail@mail.com>
2025-08-09 17:44:59 -0500

    Add Category entity and templates for category

---
+++ src/main/java/edu/pe/cibertec/spring_mvc_demo/entity/Catergory.java
@@ -0,0 +1,77 @@
+package edu.pe.cibertec.spring_mvc_demo.entity;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "categorias")
+public class Catergory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nombre", nullable = false, unique = true, length = 180)
+    private String nombre;
+    @Column(name = "descripcion", length = 250)
+    private String descripcion;
+    @Column(name = "estado", nullable = false)
+    private boolean estado;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Product> products = new ArrayList<>();
+
+    public Catergory() {
+    }
+
+    public Catergory(String descripcion, Long id, String nombre, boolean estado) {
+        this.descripcion = descripcion;
+        this.id = id;
+        this.nombre = nombre;
+        this.estado = estado;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public int getCountProducts(){
+        return  this.products.size();
+    }
+}
