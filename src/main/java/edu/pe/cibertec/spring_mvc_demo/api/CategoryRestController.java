4bcf9838b92ff658470403c973e8195e302403e2
CMENDOZA <c@mail.com>
2025-08-14 19:36:37 -0500

    Rest controller completed

--- src/main/java/edu/pe/cibertec/spring_mvc_demo/api/CategoryRestController.java
+++ src/main/java/edu/pe/cibertec/spring_mvc_demo/api/CategoryRestController.java
@@ -6,12 +6,10 @@ import edu.pe.cibertec.spring_mvc_demo.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/categories")
@@ -29,4 +27,85 @@ public class CategoryRestController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getCategory(@PathVariable Long id){
+        try {
+            Optional<Category> category = categoryService.getCategoryById(id);
+            if(category.isPresent()){
+                return  ResponseEntity.ok(category.get());
+            }else {
+                return  ResponseEntity.notFound().build();
+            }
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().build();
+
+        }catch (Exception e){
+            return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> createCategory(@RequestBody Category category){
+        try {
+            Category savedCategory = categoryService.createCategory(category);
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedCategory);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().build();
+
+        }catch (Exception e){
+            return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Category> updateCategory(@PathVariable Long id, @RequestBody Category category){
+        try {
+            Category updateddCategory = categoryService.updateCategory(id,category);
+            return ResponseEntity.ok(updateddCategory);
+        } catch (RuntimeException e) {
+            if(e.getMessage().contains("no encontrada")){
+                return  ResponseEntity.notFound().build();
+            }else {
+                return ResponseEntity.badRequest().build();
+            }
+        }
+        catch (Exception e){
+            return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Category> deleteCategory(@PathVariable Long id){
+        try {
+            boolean deleted = categoryService.deleteCategory(id);
+            if(deleted){
+                return  ResponseEntity.noContent().build();
+            }else {
+                return  ResponseEntity.notFound().build();
+            }
+        } catch (RuntimeException e) {
+            if(e.getMessage().contains("productos asociados")){
+                return  ResponseEntity.status(HttpStatus.CONFLICT).build();
+            }else {
+                return ResponseEntity.badRequest().build();
+            }
+
+        }catch (Exception e){
+            return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping("/active")
+    public ResponseEntity<List<Category>> getActiveCategories(){
+        try{
+            List<Category> categories = categoryService.getActiveCategories();
+            return ResponseEntity.ok(categories);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+
+
+
 }
